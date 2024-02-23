eddbf3b46871e3a9bb33d3f19ea55c2dcffd3256
eji_a <156595423+e-aji@users.noreply.github.com>
2024-02-23 23:41:58 +0000

    html completed, css and javascript updates

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var locationEl = document.getElementById ('location');
+var searchButtonEl = document.getElementById ('search');
+
+
+function onClickSearch(){
+
+
+
+
+}
+
+searchButtonEl.addEventListener ('click', onClickSearch);
+
